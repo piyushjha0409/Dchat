1087593f77a7de40f998e1f7f42ad387024d314a
piyush <peeyush0409@gmail.com>
2023-03-31 23:46:50 +0530

    added next and made folder structure

---
+++ components/index.js
@@ -0,0 +1,15 @@
+import Navbar from "./Navbar/Navbar";
+import Filter from "./Filter/Filter";
+import Loader from "./Loader/Loader";
+import Friend from "./Friend/Friend";
+import Model from "./Model/Model";
+import UserCard from "./UserCard/UserCard";
+import Error from "./Error/Error";
+
+export {Navbar};
+export {Filter};
+export {Loader};
+export {Friend};
+export {Model};
+export {UserCard};
+export {Error};
