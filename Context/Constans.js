485a50cbe9d532892166cad8418e07a3f0f50fc8
piyushjha0409 <peeyush0409@gmail.com>
2023-04-02 12:03:38 +0530

    chat.json moved in context

--- Context/Constans.js
+++ Context/Constans.js
@@ -1,7 +1,9 @@
+import chat from "./chat.json" 
+
 // address of the contract --->  0x5FbDB2315678afecb367f032d93F642f64180aa3
 
 //exporting the address
 export const chatContractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
 
 //exporting the abi
-export const chatContractABI = 
+export const chatContractABI = chat.abi
