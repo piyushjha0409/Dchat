80505e60d401d41574ca0de55ee3150cfacd8d73
piyushjha0409 <peeyush0409@gmail.com>
2023-04-28 19:19:05 +0530

    apiFeatures is looking good now

--- utils/apiFeatures.js
+++ utils/apiFeatures.js
@@ -20,45 +20,47 @@ export const checkIfWalletConnected = async () => {
 export const connectWallet = async () => {
   if (!window.ethereum) return console.log("Install MetaMask");
   
+  try {
   //connect the user's metamask wallet 
   const provider = new Web3(window.ethereum)
-
-  //requsting the access to the user's accounts
-  try {
-     await window.ethereum.enable()
+  const accounts = await provider.eth.getAccounts();
+  console.log(accounts[0]); //this is showing the wallet address
   } catch (error) {
     console.log(error);
   }
 
-  const accounts = await provider.eth.getAccounts();
-  console.log(accounts[0]);
 };
 
 
 
 //function for the fetching the details of the contract 
 const fetchContract = (signerOrProvider) => {
-   new ethers.Contract(chatContractAddress, chatContractABI, signerOrProvider)
+  try{
+    new ethers.Contract(chatContractAddress, chatContractABI, signerOrProvider)
+  }catch(err){
+    console.log("error fetching")
+  }
 }
 
 
 //function for connecting the contract
 export const connectingWithContract = async () => {
-  // try {
-  //   const web3modal = new Web3Modal();
-  //   const connection = await web3modal.connect();
-  //   const provider = new ethers.providers.Web3Provider(connection);
-  //   const signer = provider.getSigner();
-  //   const contract = fetchContract(signer);
-  //   return contract;
-  // } catch (error) {
-  //   console.log(error);
-  // }
-  try{
-    
-  }catch(error){
+  try {
+    const web3modal = new Web3Modal();  //creating the new instance  
+    const connection = await web3modal.connect(); //this connect method connects the user's ethereum wallet
+    const provider = new ethers.providers.Web3Provider(connection); //creating a new instance from web3providers
+    const signer = provider.getSigner();
+    const contract = fetchContract(signer);
+    return contract;
+  } catch (error) {
+    // console.log("error fetching")
     console.log(error);
   }
+  // try{
+    
+  // }catch(error){
+  //   console.log(error);
+  // }
 };
 
 //function for the converting the timestamp
